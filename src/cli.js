73777f32abf30691803ba10339d00305ac4cfe1d
Aaron Levine <ald.levine@gmail.com>
2017-11-04 15:05:34 -0400

    feat(config): Add support for config.

--- src/cli.js
+++ src/cli.js
@@ -1,19 +1,34 @@
 #!/usr/bin/env node
 
+/** @external {path} https://nodejs.org/api/path.html */
+const path = require('path');
 /** @external {minimist} https://www.npmjs.com/package/minimist */
 const minimist = require('minimist');
+/** @external {fs} https://www.npmjs.com/package/fs-extra */
+const fs = require('fs-extra');
+/** @external {promisify} https://nodejs.org/api/util.html#util_util_promisify_original */
+const {promisify} = require('util');
 /** @type {Comptroller} */
 const Comptroller = require('./comptroller');
+
+fs.pathExistsPromise = promisify(fs.pathExists);
 
 /** The cli arguments */
 const argv = minimist(process.argv.slice(2));
 
 const root = argv.root || argv.r || '.';
 const packages = argv.packages || argv.p || 'packages';
-const c = new Comptroller({root, packages});
+const configPath = argv.config || argv.c || '.comptroller.json';
+
 
 async function cli ()
 {
+  let config = {root, packages};
+  if (await fs.pathExistsPromise(path.resolve(root, configPath))) {
+    config = {...require(path.resolve(root, configPath)), ...config};
+  }
+
+  const c = new Comptroller(config);
   switch (argv._[0]) {
     case 'update':
       await c.update();
